5336cf572dd1e6d95e2d63b393fe2c39f38eef1d
subhashis patbandha <subhashispatbandha4@gmail.com>
2022-11-19 18:07:00 +0530

    about section completed

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import {FaAward} from "react-icons/fa"
+import { FaAward } from "react-icons/fa"
+import { FiUsers } from "react-icons/fi"
+import { VscFolderLibrary } from "react-icons/vsc"
 import "./about.css"
 import aboutImage from "../../assets/me.jpg"
 const About = () => {
     return (
         <section id='about'>
             <h5>get to lnow</h5>
-            <h1>about me</h1>
+            <h2>about me</h2>
             <div className="conatiner about_container">
                 <div className="about_me">
                     <div className="about_me_image">
@@ -20,7 +22,21 @@ const About = () => {
                             <h5>Experience</h5>
                             <small></small>
                         </article>
+                        <article className="about_card">
+                            <FiUsers className="about_icon" />
+                            <h5>Clients</h5>
+                            <small>200+ WORLDWIDE</small>
+                        </article>
+                        <article className="about_card">
+                            <VscFolderLibrary className="about_icon" />
+                            <h5>Projects</h5>
+                            <small>80+ projects completed</small>
+                        </article>
                     </div>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora quaerat ratione nihil quisquam recusandae vel, commodi alias iste, placeat rem maiores officia eveniet incidunt iure modi dolorem quos maxime! Ducimus!
+                    </p>
+                    <a className="btn btn-primary" href="#contact">Lei's TALK</a>
                 </div>
             </div>
         </section>
--- src/component/experience/Experience.jsx
+++ src/component/experience/Experience.jsx
@@ -1,8 +1,87 @@
 import React from 'react'
+import { BsPatchCheck } from "react-icons/bs"
 import "./experience.css"
 const Experience = () => {
     return (
-        <section id='experience'>experience</section>
+        <section id='experience'>
+            <h5>What skills I have</h5>
+            <h2>My experience</h2>
+            <div className="container experience_container">
+                <div className="experience_frontend">
+                    <h3>Frontend Development</h3>
+                    <div className="experience_content">
+                        <article className='experience_details '>
+                            <BsPatchCheck />
+                            <h4>HTML</h4>
+                            <small className='text-light'>Experienced</small>
+                        </article>
+                        <article className='experience_details '>
+                            <BsPatchCheck />
+                            <h4>CSS</h4>
+                            <small className='text-light'>Experienced</small>
+                        </article>
+                        <article className='experience_details '>
+                            <BsPatchCheck />
+                            <h4>JAVASCRIPT</h4>
+                            <small className='text-light'>Experienced</small>
+                        </article>
+                        <article className='experience_details '>
+                            <BsPatchCheck />
+                            <h4>BOOTSTRAP</h4>
+                            <small className='text-light'>Experienced</small>
+                        </article>
+                        <article className='experience_details '>
+                            <BsPatchCheck />
+                            <h4>TAILWIND</h4>
+                            <small className='text-light'>Experienced</small>
+                        </article>
+                        <article className='experience_details '>
+                            <BsPatchCheck />
+                            <h4>React</h4>
+                            <small className='text-light'>Experienced</small>
+                        </article>
+                    </div>
+                </div>
+                {/* END OF FRONTEND EXPERIENCE */}
+                <div className="experience_backend">
+                    <h3>Backend Development</h3>
+
+                    <div className="experience_content">
+                        <article className='experience_details '>
+                            <BsPatchCheck />
+                            <h4>HTML</h4>
+                            <small className='text-light'>Experienced</small>
+                        </article>
+                        <article className='experience_details '>
+                            <BsPatchCheck />
+                            <h4>CSS</h4>
+                            <small className='text-light'>Experienced</small>
+                        </article>
+                        <article className='experience_details '>
+                            <BsPatchCheck />
+                            <h4>JAVASCRIPT</h4>
+                            <small className='text-light'>Experienced</small>
+                        </article>
+                        <article className='experience_details '>
+                            <BsPatchCheck />
+                            <h4>BOOTSTRAP</h4>
+                            <small className='text-light'>Experienced</small>
+                        </article>
+                        <article className='experience_details '>
+                            <BsPatchCheck />
+                            <h4>TAILWIND</h4>
+                            <small className='text-light'>Experienced</small>
+                        </article>
+                        <article className='experience_details '>
+                            <BsPatchCheck />
+                            <h4>React</h4>
+                            <small className='text-light'>Experienced</small>
+                        </article>
+                    </div>
+                </div>
+                {/* END OF BACKEND EXPERIENCE */}
+            </div>
+        </section>
     )
 }
 
